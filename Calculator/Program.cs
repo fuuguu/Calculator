e84af12d7c92894c64b8c9149bf15180fd9de48a
linvyy <linvyy@DESKTOP-39MTSN8>
2023-06-10 17:38:00 +0200

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Голубихин Данила 22-ИСП-2/2 1.47.
+
+using Calculator;
+
+public static class Calc
+{
+    public static void Main()
+    {
+        var a = new Fraction(5, 4);
+        var b = new Fraction(1, 2);
+        Console.WriteLine(-a); 
+        Console.WriteLine(a + b);  
+        Console.WriteLine(a - b);  
+        Console.WriteLine(a * b);  
+        Console.WriteLine(a / b); 
+    }
+}
